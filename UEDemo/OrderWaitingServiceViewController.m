b240e76ad8f3a77ab71249a4f61a07c75ca74408
Arya <appleseedez@gmail.com>
2014-06-25 16:10:24 +0800

    修改了付款弹出页

--- UEDemo/OrderWaitingServiceViewController.m
+++ UEDemo/OrderWaitingServiceViewController.m
@@ -15,7 +15,11 @@
 #import "ShareService.h"
 #import "OrderPayViewController.h"
 #import "MaoRootViewController.h"
+#import "AlixPayResult.h"
+#import "AlixPayOrder.h"
 #import <UIImageView+AFNetworking.h>
+#import "DataSigner.h"
+#import "DataVerifier.h"
 @interface OrderWaitingServiceViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *lbTitle;
 @property (weak, nonatomic) IBOutlet UILabel *lbOrderName;
@@ -26,6 +30,10 @@
 @property (weak, nonatomic) IBOutlet UIView *serverListView;
 @property (weak, nonatomic) IBOutlet UIButton *rightBarButton;
 @property (weak, nonatomic) IBOutlet UILabel *lbAllPrice;
+@property (nonatomic) NSString *partnerID;
+@property (nonatomic) NSString *sellerID;
+@property (nonatomic) NSString *parterPrivateKey;
+@property (nonatomic) NSString *aliPayPubKey;
 @property (nonatomic)   ShareView *shareView;
 @end
 
@@ -46,7 +54,7 @@
     [self showEditView:@"微信朋友圈"];
 }
 - (IBAction)shareWeibo:(id)sender {
-    [[ShareService defautService] shareWithTitle:@"蓉么么" content:@"默认文案" type:@"微博"];
+    [[ShareService defautService] shareWithTitle:@"蓉么么" content:@"" type:@"微博"];
 }
 - (IBAction)shareWeixinHaoyou:(id)sender {
      [self showEditView:@"微信好友"];
@@ -75,9 +83,8 @@
     
 }
 -(void)pay{
-   OrderPayViewController *payVC= [self.storyboard instantiateViewControllerWithIdentifier:@"orderPay"];
-    payVC.order=self.order;
-    [self.navigationController pushViewController:payVC animated:YES];
+    [self getPayInfomation];
+    [self pay:nil];
 }
 -(void)share{
 
@@ -232,6 +239,79 @@
     }
 
 }
+- (IBAction)pay:(id)sender {
+    if (!self.partnerID) {
+        return;
+    }
+    if (!self.sellerID) {
+        return;
+    }
+    if (!self.parterPrivateKey) {
+        return;
+    }
+    if (!self.aliPayPubKey) {
+        return;
+    }
+    if (!self.order) {
+        return;
+    }
+    [self sendOrder];
+}
+- (NSString *)getOrderInfoFromProduct{
+    NSString *ordername=nil;
+    if ([[self.order objectForKey:@"ordertype"] isEqualToString:@"bj"]) {
+        ordername=@"蓉么么保洁服务订单";
+    }else if ([[self.order objectForKey:@"ordertype"] isEqualToString:@"gx"]){
+        ordername=@"蓉么么干洗服务订单";
+    }else if ([[self.order objectForKey:@"ordertype"] isEqualToString:@"zd"]){
+        ordername=@"蓉么么钟点工服务订单";
+    }
+    
+    
+    
+    AlixPayOrder *order = [[AlixPayOrder alloc] init];
+    order.partner = self.partnerID;
+    order.seller = self.sellerID;
+    order.productName = ordername;
+    order.productDescription = @"随便说说就好 这是文案";
+    order.amount = [NSString stringWithFormat:@"%.2f", [[self.order objectForKey:@"allprice"] floatValue]];
+    order.notifyURL = @"http%3A%2F%2F218.244.130.240:8080/eclean/alipay/noticeEcleanServer.do";
+    order.tradeNO = [self.order objectForKey:@"orderno"];
+    
+    return order.description;
+}
+-(void)getPayInfomation{
+    //这里需要做网络请求 如果请求到peiInfomation 结束流程 测试暂时写死
+    self.partnerID=PartnerID;
+    self.sellerID=SellerID;
+    self.parterPrivateKey=PartnerPrivKey;
+    self.aliPayPubKey=AlipayPubKey;
+}
+- (void)sendOrder{
+    NSString *appScheme = @"rongmeme";
+    NSString *orderInfo = [self getOrderInfoFromProduct];
+    NSString *signedStr = [self doRsa:orderInfo];
+    
+    NSLog(@"%@", signedStr);
+    
+    NSString *orderString =
+    [NSString stringWithFormat:@"%@&sign=\"%@\"&sign_type=\"%@\"", orderInfo,
+     signedStr, @"RSA"];
+    
+    [AlixLibService payOrder:orderString
+                   AndScheme:appScheme
+                     seletor:@selector(paymentResult:)
+                      target:[UIApplication sharedApplication].delegate];
+    
+    [self.navigationController popToRootViewControllerAnimated:NO];
+}
+- (NSString *)doRsa:(NSString *)orderInfo {
+    id<DataSigner> signer;
+    signer = CreateRSADataSigner(PartnerPrivKey);
+    NSString *signedString = [signer signString:orderInfo];
+    return signedString;
+}
+
 /*
 #pragma mark - Navigation
 
